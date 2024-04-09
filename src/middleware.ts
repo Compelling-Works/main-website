a04030912ac8d55878076db59f2d49471fba4eb9
Loorinho <romeojacobson@gmail.com>
2024-04-09 09:26:05 +0300

    temporarily removed auth requirements for admin

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,11 +2,11 @@ export { default } from "next-auth/middleware";
 
 export const config = {
   matcher: [
-    "/admin",
-    "/admin/projects",
-    "/admin/publications",
-    "/admin/careers-and-jobs",
-    "/admin/team",
-    "/admin/users",
+    // "/admin",
+    // "/admin/projects",
+    // "/admin/publications",
+    // "/admin/careers-and-jobs",
+    // "/admin/team",
+    // "/admin/users",
   ],
 };
